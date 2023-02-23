0826b2e3fe0b4dead7ae4477df209af8793f2622
RAM <mojicaxr@gmail.com>
2023-02-23 02:05:48 +0000

    Returned new String message object

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String message = new String("Hello World!");
+        return message;
     }
 }
